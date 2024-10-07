91504fe5d20dd165760aef03c89faaa89a66e42d
topebali <topebali97@gmail.com>
2024-10-07 11:02:16 +0100

    added incorrect format

--- infrastructure/module1/main.tf
+++ infrastructure/module1/main.tf
@@ -1,4 +1,2 @@
-resource "aws_instance" "example" {
-  ami           = "ami-0c55b159cbfafe1f0"
-  instance_type = "t2.micro"
-}
+resource "aws_instance" "example" {ami="ami-0c55b159cbfafe1f0"
+  instance_type="t2.micro"}
